53b0b35be8802860a0f5e78d7648618ce469fde1
Carolyn Oros <carolyn@insomniacdigital.com>
2014-04-06 16:36:33 -0700

    fetch model, view and controller, add partials for header and footer

--- app/controllers/fetch_controller.rb
+++ app/controllers/fetch_controller.rb
@@ -1,16 +1,138 @@
 require 'open-uri'
 require 'zlib'
+require 'yajl'
+
+# archive_url = 'http://data.githubarchive.org/2014-01-01-0.json.gz'
 
 class FetchController < ApplicationController
 
   def index
-    # Bind the form on the side bar to the registrant model
-    #@registrant = Registrant.new
+    # Bind the form to the fetch model
+    @fetch = Fetch.new
   end
 
   def create
-    # get data for specified date (default to 2014-01-01)
-    gz = open('http://data.githubarchive.org/2014-01-01-{0..23}.json.gz')
+    # make sure the URL is valid
+
+    # This url has to be in a specific format to work corectly with open_uri
+    # tokenize it to get the date only then rebuild url and loop with a range
+    # to get all the times.
+
+    # it may be better to just have the user specify the date
+    # and have the code automatically resolve the ULR, then there
+    # is less chance of errors
+    archive_url = params[:fetch][:archive_url]
+
+    # hard code based on expect conventions
+    domain = archive_url[0..29]
+    format = archive_url[-8..-1]
+
+    if domain === "http://data.githubarchive.org/" &&
+       format === ".json.gz"
+
+      # get the date only
+      match = /(\d{4}-\d{2}-\d{2})/.match(archive_url)
+      if match
+        date = match[0]
+
+        # First, get a list of urls
+        # for each hour of the specified day
+        urls = []
+        (0..23).each do | hr |
+          new_url = domain + date + "-" + hr.to_s + format
+          urls.append( new_url )
+        end
+
+        # record data
+        # this could take a while since one full day
+        # can have thousands of entries
+        puts "Got urls: "
+        puts urls
+        record_data( urls )
+
+      else
+        #TODO: change puts to proper flash messages - ok for now
+        puts "Proper date not specified. Unable to get archive data."
+        puts "Convention: yyyy-mm-dd"
+      end
+
+    else
+      puts "Invalid URL for github Archive."
+      puts "Convention: 'http://data.githubarchive.org/2014-01-01.json.gz'\n"
+    end
+
+    redirect_to root_path
   end
+
+
+  # TODO: move this to a utility file - ok for now
+  def record_data( urls )
+    skipped_urls = []
+    item_count = 0
+    urls.each do | url |
+      puts "\n\nGetting archive data for [" + url +"]:"
+
+      gz = open( url )
+      data = Zlib::GzipReader.new(gz).read
+
+      Yajl::Parser.parse(data) do | dat |
+        #TODO: add testing and error handling
+        begin
+          item_count += 1
+
+          created_at  = dat['created_at']
+          event_type  = dat['type']
+
+          # skip "GistEvent"
+          # these seem to have a lot of nil values
+          next if event_type === "GistEvent"
+
+          # skip items with no repo since they
+          # can not be displayed on report
+          next if dat['repository'] === nil
+
+          repo_name   = dat['repository']['name']
+          repo_url    = dat['repository']['url']
+          event_count = 0
+
+          # payloads have different information
+          # only get count for PushEvent
+          if event_type === "PushEvent"
+            event_count = dat['payload']['size']
+          end
+
+          # add needed data to data base
+          # not sure why we need to models, so just add to report model
+          Report.create(
+              created_at: created_at,
+              type:       event_type,
+              count:      event_count,
+              name:       repo_name,
+              url:        repo_url
+          )
+
+          puts "created_at: "  + created_at.to_s
+          puts "event_type: "  + event_type
+          puts "repo_name: "   + repo_name
+          puts "repo_url: "    + repo_url
+          puts "event_count: " + event_count.to_s
+          puts "\n\n"
+
+        rescue
+          # caught error with dat...
+          # go to the next data set
+          skipped_urls.append(dat)
+          next
+        end
+      end
+    end
+
+    puts "\nGot [" + item_count.to_s + "] entries"
+    if skipped_urls.length > 0
+      puts "Caught error for hte following urls: "
+      puts skipped_urls
+    end
+  end
+
 
 end
--- app/models/fetch.rb
+++ app/models/fetch.rb
@@ -1,7 +1,19 @@
-class Fetch < ModelBase
-  include Mongoid::Document
-  include Mongoid::Timestamps
+class Fetch
+  # Create table-less model to bind form view to model
+  # save data in Report model (why two models?)
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend  ActiveModel::Naming
 
-  field :name,  type: String
+  attr_accessor :archive_url
 
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
+
+  def persisted?
+    false
+  end
 end
