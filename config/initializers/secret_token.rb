5909efc8d3ebc01bc11671ce6614637b7168e19c
Carolyn Oros <carolyn@insomniacdigital.com>
2014-04-07 21:22:19 -0700

    remove sectet_token.rb from gitignore file. this is a test site, it does not matter.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Epoch::Application.config.secret_key_base = '378d06ea6f229170ed1eb86daca41487cf3fd9f416a7e67c31a0c20d264b169ecd03d730b3ffc812ec8c3253e52956404719dd607a27c1db92b02e80b6a84f49'
