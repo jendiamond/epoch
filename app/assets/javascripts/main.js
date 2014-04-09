fb189fa46d7a4d8f85702c70216b8dc6184442a1
Carolyn Oros <carolyn@insomniacdigital.com>
2014-04-08 17:27:22 -0700

    add javascript event handlers for graph and links (AJAX)

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("#graph-btn").on("click", graph_update);
+  $("#links-btn").on("click", links_update);
+});
+
+function graph_update() {
+  console.log( "UPDATE GRAPH");
+
+  // get date, start and end time and
+  // make ajax request for data
+
+  // update graph with top 10 reps
+  return false;
+}
+
+function links_update() {
+  console.log( "UPDATE LINKS");
+
+  // get date, start and end time and
+  // make ajax request for data
+
+  // update links table with top 25 links
+  return false;
+}
